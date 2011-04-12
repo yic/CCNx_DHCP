749d6474f38c9f85df77ec1c14b0c7128e6b2c3a
Cheng Yi <yic@email.arizona.edu>
2011-04-12 14:55:22 -0700

    test again

--- csrc/ccnd/ccnd.c
+++ csrc/ccnd/ccnd.c
@@ -1376,7 +1376,7 @@ consume_matching_interests(struct ccnd_handle *h,
              (face != NULL && p->faceid == face->faceid))) {
             if (ccn_content_matches_interest(content_msg, content_size, 0, pc,
                                              p->interest_msg, p->size, NULL)) {
-                if (face->flags & CCN_FACE_MCAST) {
+                if (f->flags & CCN_FACE_MCAST) {
                     ccnd_msg(h, "multicast ContentObject to %u (accession %llu)",
                             f->faceid, (unsigned long long)content->accession);
                 }
